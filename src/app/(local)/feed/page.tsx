919eac43afb08a0e6c409e22c1ff0af07b25b1b6
OlMinJe <Ol.MinJe@gmail.com>
2025-10-17 16:59:45 +0900

    feat(feed): 게시글 카드 임시 레이아웃 구현 및 데이터 표시

--- src/app/(local)/feed/page.tsx
+++ src/app/(local)/feed/page.tsx
@@ -1,5 +1,22 @@
 import { Posts } from '@/types/post'
 
+// 추후 common Card 컴포넌트로 교체 필요
+function PostCard({ id, content, tags, empathies, replies, createdAt }: Posts) {
+  return (
+    <section key={id}>
+      <div>{content}</div>
+      {tags.map((tag) => (
+        <span key={tag}>#{tag} </span>
+      ))}
+      <div style={{ display: 'flex' }}>
+        <div> {empathies.length}</div>
+        <div> {replies ? replies.length : 0}</div>
+        <div> {new Date(createdAt).toLocaleString()}</div>
+      </div>
+    </section>
+  )
+}
+
 export default async function Page() {
   const response = await fetch(`${process.env.NEXTAUTH_URL}/apis/posts`, {
     cache: 'no-store',
@@ -8,10 +25,8 @@ export default async function Page() {
 
   return (
     <div>
-      {posts.map((p) => (
-        <section key={p.id}>
-          <div>{p.content}</div>
-        </section>
+      {posts.map((post) => (
+        <PostCard key={post.id} {...post} />
       ))}
     </div>
   )
