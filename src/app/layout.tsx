954011c8ac9affda82548b6cc265daab87186368
Josh <59139465+Jojunhyeong@users.noreply.github.com>
2025-10-17 15:43:02 +0900

    Team c (#13)
    * chore: local style update before merge
    
    * chore: 글로벌 스타일 적용
    
    * feat: 공통 Header 컴포넌트 추가
    
    * feat(common): global css fix
    
    * feat(common): global css fix
    
    * 404 초기 버전
    
    * Adds authentication and post functionalities
    
    Implements user authentication with login, logout, and registration APIs.
    Adds functionalities for managing posts, including creating, retrieving, liking and commenting.
    Enhances security using JWT.
    
    * feat(common): add card component
    
    * fix: global.css 커스텀 유틸명 변경 및 헤더에 적용
    
    * 404 초기 버전
    
    * fix: global.css 커스텀 유틸명 변경 및 헤더에 적용
    
    * refactor(common): modify card component architecture and styling
    
    * chore: change directory path and file name
    
    ---------
    
    Co-authored-by: Lee Seung Heon <164005659+12seungheon@users.noreply.github.com>
    Co-authored-by: 2SIONN <leesion2024@gmail.com>
    Co-authored-by: Ikmyeong <boroson@naver.com>
    Co-authored-by: Nahyun <from.owol@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,17 @@
+import './globals.css'
+
 export default function RootLayout({
   children,
 }: Readonly<{
+  children: React.ReactNode
   children: React.ReactNode
 }>) {
   return (
     <html lang="ko">
       <body>{children}</body>
+    <html lang="ko">
+      <body>{children}</body>
     </html>
+  )
   )
 }
