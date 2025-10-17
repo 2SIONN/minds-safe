ca5d573eb5b1a594085f7dc36bb07f0ad088d799
shinhaeng1204@gmail.com <shinhaeng1204@gmail.com>
2025-10-17 17:07:31 +0900

    posts [id] page.tsx 만들고 apis/posts/[id] 고침

--- src/app/apis/auth/register/route.ts
+++
@@ -1,9 +0,0 @@
-// 회원가입
-// model User {
-//   id        String   @id @default(cuid())
-//   email     String   @unique
-//   password  String
-//   nickname  String?
-//   createdAt DateTime @default(now())
-// }
-// POST
