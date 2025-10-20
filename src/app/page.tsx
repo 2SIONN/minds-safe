fb1a8429db7132932494cdebdead54dbbc68ce3c
Ikmyeong <boroson@naver.com>
2025-10-20 17:09:01 +0900

    대기 문구 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,5 @@
-
-'use client';
-import { useEffect, useState } from 'react';
-
 export default function Home() {
-  const [dotCount, setDotCount] = useState(0);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setDotCount((prev: number) => (prev + 1) % 4); // 0, 1, 2, 3 → 0
-    }, 500); // 0.5초마다 실행
-
-    return () => clearInterval(interval); // 컴포넌트 언마운트 시 정리
-  }, []);
-
-  const dots = '.'.repeat(dotCount);
 
   return (
     <div className="min-h-screen flex flex-col items-center justify-center">
@@ -25,7 +11,7 @@ export default function Home() {
         
       }}></div>
       
-      <p className="mt-[1rem]">잠시만 기다려 주세요{dots}</p>
+      <p className="mt-[1rem]">잠시만 기다려 주세요!</p>
       
     </div>
 
