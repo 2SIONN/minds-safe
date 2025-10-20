397a3f2a8700023e92740f07f4338682b6a55e71
tjdwns99 <beans2853@gmail.com>
2025-10-20 16:24:17 +0900

    refactor: 게시글 상세 페이지 API 요청 파일 분리

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,30 +1,34 @@
 export const dynamic = 'force-dynamic'
 export const revalidate = 0
 
-import { headers } from 'next/headers'
 import { notFound } from 'next/navigation'
-
-async function base() {
-  const h = await headers()
-  const proto = h.get('x-forwarded-proto') ?? 'http'
-  const host = h.get('x-forwarded-host') ?? h.get('host')
-  return `${proto}://${host}`
-}
-
-async function getPostDetail(id: string) {
-  const b = await base()
-  const res = await fetch(`${b}/apis/posts/${id}`, { cache: 'no-store' })
-  console.log(res)
-  if (res.status === 404) return null
-  if (!res.ok) throw new Error(`API 실패: ${res.status}`)
-  const json = await res.json()
-  console.log(json)
-  return json
-}
+import { getPostDetail } from './queries'
 
 export default async function PostDetailPage({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params
   const post = await getPostDetail(id)
   if (!post) return notFound()
-  return <div>{post.content}</div>
+  return (
+    <>
+      <div>{post.content}</div>
+      <div className='w-1/2 flex justify-between gap-2'>
+        <input type="text" placeholder='댓글...' className='flex-10/12 border-b focus:border-1'/>
+        <button className='flex-1/12 border rounded-sm px-2 cursor-pointer'>작성</button>
+      </div>
+      <ul className='flex flex-col gap-2'>
+        {post.replies.map((reply) =>
+          <li key={reply.id} className='w-1/2 flex items-start justify-between'>
+            <div>
+              <p>{reply.author.nickname}</p> {/* 추후에 requireAuthor 함수 생성 후 글쓴이 여부 표시*/}
+              <p>{reply.body}</p>
+            </div>
+            <div className='flex gap-2'>
+              <button className='border rounded-sm px-2 cursor-pointer'>Like</button>
+              <button className='border rounded-sm px-2 cursor-pointer'>Delete</button> {/* 추후에 requireAuthor 함수 생성 후 댓글 작성자 여부에 따라 렌더링*/}
+            </div>
+          </li>
+        )}
+      </ul>
+    </>
+  )
 }
