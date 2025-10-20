db3fd89dfbb84d626a1fb4870cd7e1d464f62a40
OlMinJe <Ol.MinJe@gmail.com>
2025-10-20 16:10:39 +0900

    feat(feed-card): 게시글 목록 카드 feed-card.tsx 컴포넌트로 분리

---
+++ src/components/feed/feed-card.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import { Card, CardContent, CardFooter, CardHeader } from '@/components/common/Card'
+import { Posts } from '@/types/post'
+
+export default function FeedCard({ id, content, tags, empathies, replies, createdAt }: Posts) {
+  return (
+    <div className="p-3">
+      <Card key={id} className="min-w-sm w-sm m-0 mx-auto my-0 mb-8">
+        <CardHeader>{content}</CardHeader>
+        <CardContent>
+          {tags.map((tag) => (
+            <span key={tag}>#{tag} </span>
+          ))}
+        </CardContent>
+        <CardFooter>
+          <div style={{ display: 'flex' }}>
+            <div> {empathies.length}</div>
+            <div> {replies ? replies.length : 0}</div>
+            <div> {new Date(createdAt).toLocaleString()}</div>
+          </div>
+        </CardFooter>
+      </Card>
+    </div>
+  )
+}
