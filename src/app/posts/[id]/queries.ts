40d80d224010ad5e6d2dddea0cc3bd1b9705a394
tjdwns99 <beans2853@gmail.com>
2025-10-20 17:36:58 +0900

    chore: base Url 유틸 함수 lib 폴더 이동

--- src/app/posts/[id]/queries.ts
+++ src/app/posts/[id]/queries.ts
@@ -1,14 +1,7 @@
-import { headers } from "next/headers"
-
-export async function base() {
-  const h = await headers()
-  const proto = h.get('x-forwarded-proto') ?? 'http'
-  const host = h.get('x-forwarded-host') ?? h.get('host')
-  return `${proto}://${host}`
-}
+import { getBaseUrl } from "@/lib/http"
 
 export async function getPostDetail(id: string) {
-  const b = await base()
+  const b = await getBaseUrl()
   const res = await fetch(`${b}/apis/posts/${id}`, { cache: 'no-store' })
   console.log(res)
   if (res.status === 404) return null
