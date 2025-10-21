639090bd4c45366c8220216526a248778c9f92ca
jojunhyeong <jojh0323@naver.com>
2025-10-21 11:17:29 +0900

    refactor: Home 페이지에서 max-w, padding 공통 컨테이너로 통합

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -21,9 +21,9 @@ export default async function Home({ searchParams }: { searchParams?: Promise<{ 
   }
 
   return (
-    <>
+    <div className="mx-auto max-w-4xl px-4 sm:px-6">
       {/* 검색창 */}
-      <div className="mx-auto max-w-4xl px-4 sm:px-6 mt-6">
+      <div className="mt-6">
         <SearchFilter
           className="text-base"
           containerClassName="h-12 w-full rounded-[16px] bg-background border border-border/60 focus-within:ring-2 ring-ring/40"
@@ -32,20 +32,22 @@ export default async function Home({ searchParams }: { searchParams?: Promise<{ 
       </div>
 
       {/* 태그 리스트 */}
-      <div className="mx-auto max-w-4xl px-4 sm:px-6 flex flex-wrap gap-2 mt-4">
+      <div className="flex flex-wrap gap-2 mt-4">
         {DEFAULT_TAGS.map((t) => (
           <TagBadge key={t.value} size="md">
             {t.label}
           </TagBadge>
         ))}
       </div>
-      <Fab icon={<Plus className="w-6 h-6 " />}></Fab>
 
-      <div className="max-w-4xl px-4 sm:px-6 flex flex-col gap-2 m-0 mx-auto">
+      {/* 게시글 리스트 */}
+      <div className="flex flex-col gap-2 mt-4">
         {posts.map((p) => (
           <FeedCard key={p.id} {...p} />
         ))}
       </div>
-    </>
+
+      <Fab icon={<Plus className="w-6 h-6" />} />
+    </div>
   )
 }
