2d1e9f013498f7e222ccbdf22189cc5af59f35f9
Ikmyeong <boroson@naver.com>
2025-10-21 17:46:14 +0900

    로딩 스피너 분리

--- src/components/common/Loading.tsx
+++ src/components/common/Loading.tsx
@@ -1,19 +1,13 @@
+import Spinner from "@/components/common/Spinner";
+
 export default function Loading() {
 
   return (
     <div className="min-h-screen flex flex-col items-center justify-center">
-      <div className="w-12 h-12 border-[6] border-t-[6] border-t-[#eeeeee00] border-dotted border-[#eeeeee] rounded-full"
-      style={{
-        animation: 'spin-slow 1.25s ease-in-out infinite',
-        maskImage: 'conic-gradient(transparent 0deg, #000000ff 180deg )',
-        WebkitMaskImage: 'conic-gradient(transparent 0deg, #000000ff 360deg)',
-        
-      }}></div>
-      
+      <Spinner></Spinner>
       <p className="mt-[1rem]">잠시만 기다려 주세요!</p>
       
     </div>
 
   );
 }
-
