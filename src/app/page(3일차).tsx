a09db831c2bce79282d493bc6ec1ba7c583455ed
Ikmyeong <boroson@naver.com>
2025-10-21 17:41:03 +0900

    로딩 스피너 컴포넌트 분리

--- src/app/page(3일차).tsx
+++ src/app/page(3일차).tsx
@@ -1,14 +1,14 @@
 import Spinner from "@/components/common/Spinner";
 
-export default function Loading() {
-
+export default function Home() {
+  
   return (
     <div className="min-h-screen flex flex-col items-center justify-center">
       <Spinner></Spinner>
+      
       <p className="mt-[1rem]">잠시만 기다려 주세요!</p>
       
     </div>
 
   );
 }
-
