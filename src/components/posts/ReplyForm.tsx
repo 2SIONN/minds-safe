949b37badc207767ef706928080538ab2388afc4
tjdwns99 <beans2853@gmail.com>
2025-10-22 14:08:13 +0900

    feat: 댓글 입력창 RHF 적용

--- src/components/posts/ReplyForm.tsx
+++ src/components/posts/ReplyForm.tsx
@@ -3,19 +3,33 @@
 import { ChangeEvent, useState } from "react";
 import Button from "../common/Button";
 import Textarea from "../common/Textarea";
+import { z } from "zod";
+import { replyCreateSchema } from "@/lib/validators";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+type ReplyInput = z.infer<typeof replyCreateSchema>
 
 export default function ReplyForm() {
-  const [reply, setReply] = useState('');
-  const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => { setReply(e.target.value) }
+  const { register, handleSubmit, formState: { errors, isSubmitting }, reset, watch } = useForm<ReplyInput>({
+    resolver: zodResolver(replyCreateSchema),
+    defaultValues: {
+      body: ''
+    }
+  })
+
+  const len = watch('body').length
+
   return (
-    <div className="w-full flex items-end gap-4">
+    <form className="w-full flex items-end gap-4"> {/*Subbit handler 추가 예정*/}
       <Textarea
         wrapperClassName="flex-1"
         textareaClassName="rounded-md"
         placeholder="따뜻한 응원을 남겨보세요..."
-        onChange={handleChange}
+        {...register('body')}
       />
-      <Button disabled={!reply}>전송</Button>
-    </div>
+      {/* 길이 표시 및 에러 상태 표시 추가 예정 */}
+      <Button disabled={len === 0 || isSubmitting}>전송</Button>
+    </form>
   )
 }
