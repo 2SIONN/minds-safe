9df00bae39f6dd8debf2d7a56489d965eab5ca46
OlMinJe <Ol.MinJe@gmail.com>
2025-10-22 12:02:22 +0900

    style(feed-card): 메인 카드 리스트 레이아웃 스타일 수정 및 정렬 부분 공간 확보

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -33,10 +33,16 @@ export default async function Home({ searchParams }: { searchParams?: Promise<{ 
             ))}
           </div>
 
-          {/* 게시글 리스트 / 빈 상태 */}
-          <Suspense key={q || ''} fallback={<Loading />}>
-            <AllPosts q={q || ''} />
-          </Suspense>
+          <main className="max-w-4xl mx-auto p-4">
+            <div className="flex justify-end mb-4">
+              {/* 정렬 */}
+              <div></div>
+            </div>
+            {/* 게시글 리스트 / 빈 상태 */}
+            <Suspense key={q || ''} fallback={<Loading />}>
+              <AllPosts q={q || ''} />
+            </Suspense>
+          </main>
         </div>
       </section>
       <Fab icon={<Plus className="w-6 h-6" />} />
