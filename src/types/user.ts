5abd742e55a64e9a226fc047f69162c1381fa45a
tjdwns99 <beans2853@gmail.com>
2025-10-23 18:11:19 +0900

    Team A (#133)
    * feat: 로그인/회원가입 유효성 추가 (#15)
    
    * Feature/posts id page (#17)
    
    * posts [id] page.tsx 만들고 apis/posts/[id] 고침
    
    * Removes unused env variables
    
    Removes AUTH_SECRET and NEXTAUTH_URL env variables which are no longer in use.
    Updates JWT_SECRET with a placeholder value to encourage users to set a strong secret.
    
    * feat: auth 상태 관리를 위한 zustand store 추가
    
    * 게시글 상세 UI 초안 작성
    
    * components 따로 뺐어요
    
    * feat(write): connect Zod, 기본 틀 작성
    
    * refactor: 게시글 상세 페이지 API 요청 파일 분리
    
    * types: 게시글 관련 타입 정의
    * Post, Reply, Empathy, User 타입 정의
    
    * feat: 회원가입 폼 추가
    
    * chore: base Url 유틸 함수 lib 폴더 이동
    
    * chore: remove files outside (main)
    
    * chore: remove files outside (main)
    * Removed query files outside (main)
    
    * reafactor: 공통컴포넌트적용
    
    * feat: 댓글창 초기 구현
    * 댓글 리스트 UI 구현
    * 댓글 길이에 따른 Truncate (더보기 & 간략히 보기 구현)
    
    * feat:회원가입 페이지 기능 구현
    
    * feat: 회원가입 성공시 로그인 페이지로 이동
    
    * refactor: register 경로수정
    
    * feat: 댓글 입력창 추가
    
    * design: 댓글 리스트 UI 조정
    
    * feat(post): FAB→작성 모달 연결
    
    * feat: 댓글 입력창 RHF 적용
    
    * feat(page) : FAB/모달 교체
    
    * 게시글 모달 UI 적용 완료
    
    * files: remove unused files
    
    * feat: 댓글 입력창 게시글 상세 모달에 추가
    * 게시글 상세 전체 UI 조정
    
    * validators 통합 및 경로 수정
    
    * feat:비어있으면 익명
    
    * feat:login ui
    
    * feat/login
    
    * Revert "feat/login"
    
    This reverts commit 573fc3c84614e96adae78e659398801cf42afa1c.
    
    * feature: 로그인기능구현
    
    * 좋아요 버튼 구현
    
    * 좋아요 버튼 기능 구현(낙관적업데이트 아님)
    
    * refactor: 댓글 리액트 쿼리 적용
    * 댓글 api 호출 파일 분리
    * 댓글 쿼리 훅 추가
    
    * feat: 좋아요 수 및 댓글 작성자 표기
    
    * feat: 댓글 작성 기능
    * 낙관적 업데이트
    * mutation payload 타입 정의 (ReplyPayload)
    
    * refacotr
    
    * chore: sync with dev
    
    ---------
    
    Co-authored-by: Hyunji <hyunhyun0096@naver.com>
    Co-authored-by: shinhaeng1204 <129822507+shinhaeng1204@users.noreply.github.com>
    Co-authored-by: 2SIONN <leesion2024@gmail.com>
    Co-authored-by: shinhaeng1204@gmail.com <shinhaeng1204@gmail.com>
    Co-authored-by: asdfdk123 <cwh0607@naver.com>
    Co-authored-by: asdfdk123 <90261274+asdfdk123@users.noreply.github.com>

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,12 +1,14 @@
-import { Post, Reply, Empathy } from "./post"
+import { Post, Reply, Empathy } from './post'
 
 export type User = {
   id: string
   email: string
-  password:  string
+  password: string
   nickname?: string
   createdAt: string
   posts: Post[]
   replies: Reply[]
   empathies: Empathy[]
 }
+
+export type Author = Omit<User, 'posts' | 'replies' | 'empathies'>
