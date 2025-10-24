caa53090a3dffbf0c0dee4f1f162d4c942c05dc0
BES9507 <BES9507@naver.com>
2025-10-24 17:07:35 +0900

    [B] feat(NicknameSection): STORAGE 구현

--- src/app/(main)/mypage/page.tsx
+++ src/app/(main)/mypage/page.tsx
@@ -8,7 +8,7 @@ export default function Page() {
   return (
     <main className="min-h-screen text-zinc-100">
       <div className="mx-auto max-w-5xl px-6 py-10">
-        <NicknameSection initialName="익명" />
+        <NicknameSection/>
         <MypagePostCard />
       <MyReplies />
       </div>
