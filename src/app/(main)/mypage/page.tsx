989e7c465a596fbf5c65ec71f4781d9025e6cfca
박총명 <qkrri56@naver.com>
2025-10-24 15:59:36 +0900

    feat : mypage CSS 수정 및 삭제 API 적용

--- src/app/(main)/mypage/page.tsx
+++ src/app/(main)/mypage/page.tsx
@@ -6,8 +6,8 @@ import MyReplies from '@/components/mypage/MyReplies.tsx'
 
 export default function Page() {
   return (
-    <main className="min-h-screen p-4">
-      <div className="max-w-4xl mx-auto">
+    <main className="min-h-screen pb-4">
+      <div className="max-w-4xl px-4 sm:px-6 mx-auto">
         <NicknameSection initialName="익명" />
         <MypagePostCard />
         <MyReplies />
