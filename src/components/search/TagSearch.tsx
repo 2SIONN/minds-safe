8320497c60775b6b656ea51025cbf9cc3ab8182d
cji <gugugeul273@gmail.com>
2025-10-24 13:50:33 +0900

    feat(tag):tag api로 가져오기 기능 구현

--- src/components/search/TagSearch.tsx
+++ src/components/search/TagSearch.tsx
@@ -1,13 +1,47 @@
-import TagBadge, { DEFAULT_TAGS } from '../common/TagBadge'
+'use client'
+import { useEffect, useState } from 'react'
+import TagBadge from '@/components/common/TagBadge'
+
+type TagType = {
+  tag: string
+  count: number
+}
 
 export default function TagSearch() {
+  const [tags, setTags] = useState<TagType[]>([])
+  const [selectedTag, setSelectedTag] = useState<string | null>(null)
+
+  useEffect(() => {
+    async function fetchTags() {
+      try {
+        const res = await fetch('/apis/tags')
+        const data = await res.json()
+
+        if (data.success) {
+          setTags(data.items)
+        }
+      } catch (e) {
+        console.log(e)
+      }
+    }
+    fetchTags()
+  }, [])
+
+  const onClickTag = (tag: string) => {
+    setSelectedTag(tag === selectedTag ? null : tag) //선택 해제 기능 포함
+  }
   return (
     <>
       {/* 태그 리스트 */}
       <div className="mx-auto max-w-4xl px-4 sm:px-6 flex flex-wrap gap-2 mt-4">
-        {DEFAULT_TAGS.map((t) => (
-          <TagBadge key={t.value} size="md">
-            {t.label}
+        {tags.map((t) => (
+          <TagBadge
+            key={t.tag}
+            size="md"
+            onClick={() => onClickTag(t.tag)}
+            selected={selectedTag === t.tag}
+          >
+            {t.tag}
           </TagBadge>
         ))}
       </div>
