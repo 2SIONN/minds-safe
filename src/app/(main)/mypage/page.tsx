76ee480a1616484fddb8bfd694fec701a5a19730
BES9507 <BES9507@naver.com>
2025-10-24 17:17:35 +0900

    다시 PR합니다

--- src/app/(main)/mypage/page.tsx
+++ src/app/(main)/mypage/page.tsx
@@ -8,7 +8,7 @@ export default function Page() {
   return (
     <main className="min-h-screen text-zinc-100">
       <div className="mx-auto max-w-5xl px-6 py-10">
-        <NicknameSection/>
+        <NicknameSection initialName="익명" />
         <MypagePostCard />
       <MyReplies />
       </div>
