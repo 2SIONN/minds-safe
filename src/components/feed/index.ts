d16349353ebd8894b346dd72d4939917fdcb21d8
OlMinJe <Ol.MinJe@gmail.com>
2025-10-27 15:30:20 +0900

    feat(feed-list): feed 컴포넌트 index 파일 생성

---
+++ src/components/feed/index.ts
@@ -0,0 +1,4 @@
+export { default as AllPosts } from '@/components/feed/AllPosts'
+export { default as FeedCard } from '@/components/feed/FeedCard'
+export { default as FeedListSkeleton } from '@/components/feed/FeedListSkeleton'
+export { default as FeedTags } from '@/components/feed/FeedTags'
