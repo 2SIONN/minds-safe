480a70e966ccabd0b39535a7a690db1100229d86
Ikmyeong <boroson@naver.com>
2025-10-27 17:24:28 +0900

    스켈레톤 UI, 404 반응형 수정

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,12 +4,12 @@ export default function NotFound() {
   return (
     <div className="min-h-screen flex flex-col">
       <div className="mt-[5rem] flex justify-center">
-        <h1 className="font-sans font-bold text-[48px]">404 Not Found 😢</h1>
+        <h1 className="font-sans font-bold max-[400px]:text-[27px] max-[600px]:text-[36px] text-[48px]">404 Not Found 😢</h1>
       </div>
       
       <div className="mt-[-5rem] flex flex-1 flex-col gap-10 items-center justify-center">
-        <p className="text-[24px]">페이지를 찾을 수 없습니다.</p>
-        <Link className="text-[#006699] hover:underline" href="/">홈으로 돌아가기</Link>
+        <p className="max-[400px]:text-[16px] max-[600px]:text-[20px] text-[24px]">페이지를 찾을 수 없습니다.</p>
+        <Link className="max-[400px]:text-[12px] max-[600px]:text-[14px] text-[#006699] hover:underline" href="/">홈으로 돌아가기</Link>
       </div>
     </div>
   )
--- src/components/common/CardSkeleton.tsx
+++ src/components/common/CardSkeleton.tsx
@@ -2,22 +2,22 @@ import { Card, CardContent, CardFooter, CardHeader } from '@/components/common/C
 
 export default function CardSkeleton() {
   return (
-    <Card className="w-full skeleton mt-4">
-      <CardHeader className="p-5 pb-0 ">
-        <span className="px-60 bg-muted/50 rounded-[14px]"></span>
+    <Card className="backdrop-blur-xl w-full skeleton mb-4">
+      <CardHeader className="mt-1.5 px-5 py-4">
+        <span className="px-60 max-[600px]:px-30 bg-muted/50 rounded-[14px]"></span>
       </CardHeader>
-      <CardContent className="mt-0.5 px-5 py-3 ">
-        <span className="mr-2 px-[25px] bg-muted/50 rounded-[14px]"></span>
-        <span className="mr-2 px-[25px] bg-muted/50 rounded-[14px]"></span>
-        <span className="mr-2 px-[25px] bg-muted/50 rounded-[14px]"></span>
+      <CardContent className="mt-[-24] px-5">
+        <span className="mr-2 pb-0.5 px-[25px] bg-muted/50 rounded-[14px]"></span>
+        <span className="mr-2 pb-0.5 px-[25px] bg-muted/50 rounded-[14px]"></span>
+        <span className="mr-2 pb-0.5 px-[25px] bg-muted/50 rounded-[14px]"></span>
       </CardContent>
-      <CardFooter className="gitp-5 pt-0 text-muted-foreground text-sm">
+      <CardFooter className=" mt-[-25px] px-5  text-muted-foreground text-sm">
         <div className="w-full flex justify-between">
-          <div className="h-[20px] px-[16px] bg-muted/50 rounded-[14px]"></div>
-          <div className="flex gap-4">
-            <div className="mr-[-1] h-[20px] px-[12px] bg-muted/50 rounded-[14px]"></div>
-            <div className="h-[20px] px-[18px] bg-muted/50 rounded-[14px]"></div>
-            <div className="h-[20px] px-[18px] bg-muted/50 rounded-[14px]"></div>
+          <div className="h-[22px] px-[20px] bg-muted/50 rounded-[14px]"></div>
+          <div className="flex gap-3.5">
+            <div className="mr-[-1] h-[20px] px-[20px] bg-muted/50 rounded-[14px]"></div>
+            <div className="h-[20px] px-[20px] bg-muted/50 rounded-[14px]"></div>
+            <div className="h-[20px] px-[20px] bg-muted/50 rounded-[14px]"></div>
           </div>
         </div>
       </CardFooter>
