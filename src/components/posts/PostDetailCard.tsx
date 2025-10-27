2cbbcdf3ba260a59be61ca719f6e24f1615d7eb9
tjdwns99 <beans2853@gmail.com>
2025-10-27 16:07:02 +0900

    feat: 좋아요 토글 적용 - 게시글 상세 페이지 호출 시 리액트 쿼리 적용 필요 - 게시글 피드 및 댓글 좋아요 동작 확인

--- src/components/posts/PostDetailCard.tsx
+++ src/components/posts/PostDetailCard.tsx
@@ -8,6 +8,7 @@ import ReplyForm from './ReplyForm'
 import LikeButton from './LikeButton'
 import NickName from './NickName'
 import PostTags from './PostTags'
+import { useAuthStore } from '@/store/useAuthStore'
 
 type Props = {
   open: boolean
@@ -16,7 +17,8 @@ type Props = {
 }
 
 export default function PostDetailCard({ open, onClose, post }: Props) {
-  const currentUserId = localStorage.getItem('userId') || ''
+  const { user } = useAuthStore();
+  const currentUserId = localStorage.getItem('userId') || user?.id || ''
   const initiallyLiked = post?.empathies?.some((e) => e.userId === currentUserId) ?? false
   return (
     <Modal open={open} onClose={onClose} size="2xl" closeOnBackdrop>
@@ -42,9 +44,10 @@ export default function PostDetailCard({ open, onClose, post }: Props) {
                 const likeCount = post.empathies?.length ?? 0 // 기본값 0
                 return (
                   <LikeButton
+                    type='POST'
                     id={post.id}
-                    initialActive={initiallyLiked}
-                    initialCount={likeCount}
+                    active={initiallyLiked}
+                    count={likeCount}
                   />
                 )
               })()}
