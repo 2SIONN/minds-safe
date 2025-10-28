993e0b817b4ec7b285b1c3fc6ac11b1d822258f7
Ikmyeong <boroson@naver.com>
2025-10-28 17:43:39 +0900

    스켈레톤 UI 반응형 구현

--- src/components/common/CardSkeleton.tsx
+++ src/components/common/CardSkeleton.tsx
@@ -11,13 +11,13 @@ export default function CardSkeleton() {
         <span className="card-tags"></span>
         <span className="invisible sm:visible card-tags"></span>
       </CardContent>
-      <CardFooter className="pt-3 -mb-0.5 px-5 text-muted-foreground text-sm">
+      <CardFooter className="pt-3 mb-0.25 px-5 text-muted-foreground text-sm">
         <div className="w-full flex justify-between">
           <div className="card-footer w-14"></div>
           <div className="flex gap-4">
-            <div className="card-footer w-10"></div>
-            <div className="card-footer w-10"></div>
-            <div className="card-footer w-12"></div>
+            <div className="card-footer w-6 sm:w-10"></div>
+            <div className="card-footer w-6 sm:w-10"></div>
+            <div className="card-footer w-8 sm:w-12"></div>
           </div>
         </div>
       </CardFooter>
