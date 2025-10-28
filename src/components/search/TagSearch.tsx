75818cdbc4cfb28c62bf66e66cc8b604c54a474e
cji <gugugeul273@gmail.com>
2025-10-28 16:54:12 +0900

    feat(tag):태그 수정

--- src/components/search/TagSearch.tsx
+++ src/components/search/TagSearch.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { useEffect, useState } from 'react'
 import TagBadge from '@/components/common/TagBadge'
-import { useSearchParams } from 'next/navigation'
+import { useRouter, useSearchParams } from 'next/navigation'
 
 type TagType = {
   tag: string
@@ -10,9 +10,10 @@ type TagType = {
 
 export default function TagSearch() {
   const [tags, setTags] = useState<TagType[]>([])
-  const [selectedTag, setSelectedTag] = useState<string[]>(['전체'])
+  const [selectedTag, setSelectedTag] = useState<string>('전체')
   const searchParams = useSearchParams()
-  const q = searchParams.get('q')
+  const router = useRouter()
+  const search = searchParams.get('q')
   const MaxSelect = 5
 
   useEffect(() => {
@@ -22,7 +23,7 @@ export default function TagSearch() {
         const data = await res.json()
 
         if (data.success) {
-          const topTags = data.items.slice(0)
+          const topTags = data.items.slice(0, 10)
           setTags([{ tag: '전체', count: 0 }, ...topTags])
         }
       } catch (e) {
@@ -31,18 +32,40 @@ export default function TagSearch() {
     }
     fetchTags()
   }, [])
+  //selected를 받아온다
+  useEffect(() => {
+    async function fetchPost() {
+      try {
+        const apiUrl =
+          selectedTag === '전체'
+            ? '/apis/posts'
+            : `/apis/posts?tag=${encodeURIComponent(selectedTag)}`
 
-  const onClickTag = (tag: string) => {}
+        const res = await fetch('/apis/posts')
+        const data = await res.json()
+        console.log(data)
+      } catch (e) {
+        console.error(e)
+      }
+    }
+    fetchPost()
+  }, [])
+
+  const onClickTag = (tag: string) => {
+    const selec = selectedTag ? '전체' : tag
+    setSelectedTag(selec)
+    console.log(selec)
+  }
   return (
     <>
       {/* 태그 리스트 */}
-      <div className="mx-auto max-w-4xl px-4 sm:px-6 flex flex-wrap gap-2 mt-4">
+      <div className="mx-auto max-w-4xl mb-4 flex flex-wrap gap-2 mt-4">
         {tags.map((t) => (
           <TagBadge
             key={t.tag}
             size="md"
             onClick={() => onClickTag(t.tag)}
-            selected={selectedTag.includes(t.tag)}
+            selected={selectedTag === t.tag}
           >
             {t.tag}
           </TagBadge>
