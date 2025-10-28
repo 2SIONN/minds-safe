010f4615e395dc5cd61672404d1441e35198accb
Ikmyeong <boroson@naver.com>
2025-10-28 11:15:24 +0900

    스켈레톤 UI 틀 수정

--- src/components/common/CardSkeleton.tsx
+++ src/components/common/CardSkeleton.tsx
@@ -7,17 +7,17 @@ export default function CardSkeleton() {
         <span className="px-60 bg-muted/50 rounded-[14px]"></span>
       </CardHeader>
       <CardContent className="mt-0.5 px-5 py-3 ">
-        <span className="mr-2 px-[25px] bg-muted/50 rounded-[14px]"></span>
-        <span className="mr-2 px-[25px] bg-muted/50 rounded-[14px]"></span>
-        <span className="mr-2 px-[25px] bg-muted/50 rounded-[14px]"></span>
+        <span className="mr-2 pb-0.5 px-6.25 bg-muted/50 rounded-[14px]"></span>
+        <span className="mr-2 pb-0.5 px-6.25 bg-muted/50 rounded-[14px]"></span>
+        <span className="mr-2 pb-0.5 px-6.25 bg-muted/50 rounded-[14px]"></span>
       </CardContent>
-      <CardFooter className="gitp-5 pt-0 text-muted-foreground text-sm">
+      <CardFooter className="mt-[-25] px-5 text-muted-foreground text-sm">
         <div className="w-full flex justify-between">
-          <div className="h-5 px-4 bg-muted/50 rounded-[14px]"></div>
-          <div className="flex gap-4">
-            <div className="mr-[-1] h-5 px-3 bg-muted/50 rounded-[14px]"></div>
-            <div className="h-5 px-[18px] bg-muted/50 rounded-[14px]"></div>
-            <div className="h-5 px-[18px] bg-muted/50 rounded-[14px]"></div>
+          <div className="h-5.5 px-5 bg-muted/50 rounded-[14px]"></div>
+          <div className="flex gap-3.5">
+            <div className="mr-[-1] h-5 px-5 bg-muted/50 rounded-[14px]"></div>
+            <div className="h-5 px-5 bg-muted/50 rounded-[14px]"></div>
+            <div className="h-5 px-5 bg-muted/50 rounded-[14px]"></div>
           </div>
         </div>
       </CardFooter>
