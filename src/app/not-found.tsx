7f04b35e893569ad7bc110913d8d67ac3eb5abe8
Ikmyeong <boroson@naver.com>
2025-10-28 11:36:11 +0900

    404 반응형 Tailwind 수정

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,12 +4,13 @@ export default function NotFound() {
   return (
     <div className="min-h-screen flex flex-col">
       <div className="mt-20 flex justify-center">
-        <h1 className="font-sans font-bold max-[300px]:text-2xl max-[600px]:text-3xl text-5xl">404 Not Found</h1><h1 className="min-[300px]:text-3xl min-[600px]:text-5xl text-[0px]">😢</h1>
+        <h1 className="font-sans font-bold text-2xl min-sm:text-4xl min-lg:text-5xl">404 Not Found</h1>
+        <h1 className="min-sm:text-4xl min-lg:text-5xl text-[0px]">😢</h1>
       </div>
       
       <div className="-mt-12 flex flex-1 flex-col gap-10 items-center justify-center">
-        <p className="max-[300px]:text-sm max-[600px]:text-lg text-xl">페이지를 찾을 수 없습니다.</p>
-        <Link className="max-[300px]:text-xs max-[600px]:text-sm text-[#006699] hover:underline" href="/">홈으로 돌아가기</Link>
+        <p className="text-sm min-sm:text-base min-lg:text-lg">페이지를 찾을 수 없습니다.</p>
+        <Link className="text-xs min-sm:text-sm min-lg:text-base text-[#006699] hover:underline" href="/">홈으로 돌아가기</Link>
       </div>
     </div>
   )
