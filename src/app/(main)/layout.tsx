f7cfdadc5b4cad588d0b6157f7d7ac7abb90effc
jojunhyeong <jojh0323@naver.com>
2025-10-29 11:50:22 +0900

    refactor: 헤더 hover 속성 기획대로 변경

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -5,19 +5,19 @@ import Toast from '@/components/common/Toast'
 import Link from 'next/link'
 import { usePathname, useRouter } from 'next/navigation'
 import { ReactNode } from 'react'
-import { useAuthStore } from '@/store/useAuthStore' // ✅ Zustand 스토어 가져오기
+import { useAuthStore } from '@/store/useAuthStore' // Zustand 스토어 가져오기
 import { LogOut } from 'lucide-react'
 import { Button } from '@/components/common'
 
 export default function HomePage({ children }: { children: ReactNode }) {
   const pathname = usePathname()
   const router = useRouter()
-  const { isAuthed, logout } = useAuthStore() // ✅ 로그인 상태와 로그아웃 함수 가져오기
+  const { isAuthed, logout } = useAuthStore() // 로그인 상태와 로그아웃 함수 가져오기
 
   const handleLogout = async () => {
     try {
       await fetch('/apis/auth/logout', { method: 'POST', credentials: 'include' })
-      logout() // ✅ Zustand 스토어 상태 갱신
+      logout() // Zustand 스토어 상태 갱신
       router.refresh()
       router.push('/login')
     } catch (e) {
@@ -42,21 +42,18 @@ export default function HomePage({ children }: { children: ReactNode }) {
           title="고민타파"
           titleClassName="text-lg sm:text-xl font-extrabold gradient-text"
           right={
-            <div className="flex items-center gap-3 text-muted-foreground text-sm">
-              <span className="hidden sm:inline">익명</span>
-              <Link href="/mypage" className="text-primary hover:underline underline-offset-4">
-                마이페이지
-              </Link>
-
-              {/* ✅ 로그인 여부에 따라 버튼 분기 */}
+            <div className="flex items-center gap-3 text-muted-foreground text-sm rounded-md ">
+              {/* 로그인 여부에 따라 버튼 분기 */}
               {isAuthed ? (
-                <Button
-                  onClick={handleLogout}
-                  variant='ghost'
-                  size='sm'
-                >
-                  <LogOut className='w-4 h-4'/>
-                </Button>
+                <>
+                  <span className="hidden sm:inline">익명</span>
+                  <Link href="/mypage" className="inline-flex items-center justify-center gap-2 whitespace-nowrap px-3 py-2 rounded-xl  text-primary hover:bg-accent hover:text-accent-foreground">
+                    마이페이지
+                  </Link>
+                  <Button onClick={handleLogout} variant="ghost" size="sm" className='hover:bg-accent hover:text-accent-foreground'>
+                    <LogOut className="w-4 h-4" />
+                  </Button>
+                </>
               ) : (
                 <Link href="/login" className="p-2 rounded-lg hover:bg-muted/40">
                   로그인
